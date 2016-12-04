b0b5b86eb3dfd85b399457a84ad1f18cea91b220
mkozikowsk <mkmarcinkozikowski@gmail.com>
2016-12-04 10:36:46 -0800

    add entity borrows

--- src/main/webapp/app/entities/books/books.state.js
+++ src/main/webapp/app/entities/books/books.state.js
@@ -134,6 +134,43 @@
                 });
             }]
         })
+        .state('books.show', {
+                    parent: 'books',
+                    url: '/{id}/show',
+                    data: {
+                        authorities: ['ROLE_USER']
+                    },
+                    onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                        $uibModal.open({
+                            templateUrl: 'app/entities/books/books-dialog.html',
+                            controller: 'BooksDialogController',
+                            controllerAs: 'vm',
+                            backdrop: 'static',
+                            size: 'lg',
+                            resolve: {
+                                entity: function () {
+                                    return {
+                                        isbn: null,
+                                        title: null,
+                                        author: null,
+                                        publication_year: null,
+                                        publisher: null,
+                                        image_s: null,
+                                        image_m: null,
+                                        image_l: null,
+                                        id: null
+                                    };
+
+                                }
+                            }
+                        }).result.then(function() {
+                            $state.go('books', null, { reload: 'books' });
+                        }, function() {
+                            $state.go('books');
+                        });
+                    }]
+                })
+
         .state('books.edit', {
             parent: 'books',
             url: '/{id}/edit',
